3404958bc2a0299bdc09f446daee4d8c1792bb6e
Gary A. Stafford <garystafford@rochester.rr.com>
2017-09-21 10:40:36 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Docker Logging on AWS Demo
---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# disconect from AIP Docker dameon
+# unset DOCKER_TLS_VERIFY DOCKER_CERT_PATH DOCKER_HOST
+
+export WORKER_NODE_1=ip-172-31-23-24.ec2.internal
+export WORKER_NODE_2=ip-172-31-4-133.ec2.internal
+export DEPLOY_ENV=Development
+export ELK_IP=172.31.23.24
+# deploy to aip dev environment
+# docker stack remove elk-demo
+docker stack deploy --compose-file=docker-compose.yml --with-registry-auth elk-demo
