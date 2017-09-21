f1c18bc39e572be390885f365fe267bb77acae3d
Gary A. Stafford <garystafford@rochester.rr.com>
2017-09-21 19:31:06 -0400

    Refactor config

--- build.sh
+++ build.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-# disconect from AIP Docker dameon
-# unset DOCKER_TLS_VERIFY DOCKER_CERT_PATH DOCKER_HOST
+export MANAGER_NODE=34.206.1.241
+ssh -i "~/.ssh/elk-demo" -NL localhost:2375:/var/run/docker.sock docker@${MANAGER_NODE} &
+export DOCKER_HOST=tcp://127.0.0.1:2375
+
 
 export WORKER_NODE_1=ip-172-31-23-24.ec2.internal
 export WORKER_NODE_2=ip-172-31-38-25.ec2.internal
 export DEPLOY_ENV=Development
 export ELK_IP=172.31.23.24
-# deploy to aip dev environment
-# docker stack remove elk-demo
+
+echo "Deploying stack..."
 docker stack deploy --compose-file=docker-compose.yml --with-registry-auth elk-demo
+
+echo "waiting for loggers to come up..."
+sleep 10s
+
+echo "starting hello services now that loggers are up..."
+docker service update --force elk-demo_hello-logspout
+docker service update --force elk-demo_hello-gelf
+docker service update --force elk-demo_hello-fluentd
