d9b415de83353d884eb79ff0a30f4d1ee4a6605c
Gary A. Stafford <garystafford@rochester.rr.com>
2017-09-26 22:25:31 -0400

    Refactor config

--- stack_deploy.sh
+++ stack_deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# export MANAGER_NODE=34.206.1.241
-# ssh -i "~/.ssh/elk-demo" -NL localhost:2375:/var/run/docker.sock docker@${MANAGER_NODE}
-# export DOCKER_HOST=tcp://127.0.0.1:2375
-
-
 export WORKER_NODE_3='ip-172-31-5-65.ec2.internal'
 export DEPLOY_ENV='development'
 export ELK_IP='172.31.5.65'
